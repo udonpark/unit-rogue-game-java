4d7f69200db915b152b6beda367993afbb5d16dd
yoo <ykog0002@student.monash.edu>
2021-09-19 00:20:16 +0800

    Weapons created

---
+++ src/weapon/GiantAxe.java
@@ -0,0 +1,9 @@
+package weapon;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class GiantAxe extends WeaponItem {
+    public GiantAxe(){
+        super("Giant Axe", 'G', 50, "hits", 80);
+    }
+}
---
+++ src/weapon/StormRuler.java
@@ -0,0 +1,10 @@
+package weapon;
+
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class StormRuler extends WeaponItem {
+    public StormRuler(){
+        super("Storm Ruler", 'S', 70, "hits", 70);
+    }
+
+}
