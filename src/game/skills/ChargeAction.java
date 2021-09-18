901ca6710afd3af9414f9d695b9e29e0386abefe
yoo <ykog0002@student.monash.edu>
2021-09-19 00:36:07 +0800

    Charge created

---
+++ src/game/skills/ChargeAction.java
@@ -0,0 +1,28 @@
+package game.skills;
+
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.WeaponAction;
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class ChargeAction extends WeaponAction {
+
+    /**
+     * Constructor
+     *
+     * @param weaponItem the weapon item that has capabilities
+     */
+    public ChargeAction(WeaponItem weaponItem) {
+        super(weaponItem);
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
+}
