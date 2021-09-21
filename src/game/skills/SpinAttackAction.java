cf4531240cffe060342b22c48dc035919a8a51a3
yoo <ykog0002@student.monash.edu>
2021-09-21 23:30:30 +0800

    WeaponActions created

---
+++ src/game/skills/SpinAttackAction.java
@@ -0,0 +1,28 @@
+package game.skills;
+
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.WeaponAction;
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class SpinAttackAction extends WeaponAction {
+    /**
+     * Constructor
+     *
+     * @param weaponItem the weapon item that has capabilities
+     */
+    public SpinAttackAction(WeaponItem weaponItem) {
+        super(weaponItem);
+    }
+
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
+}
