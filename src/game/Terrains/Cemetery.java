8ee6e321dc162e0bf337e4e9ee4e21e7d6863e41
hriz0002 <hriz0002@student.monash.edu>
2021-09-26 21:47:13 +0800

    Javadoc for Cemetery and Valley

--- src/game/Terrains/Cemetery.java
+++ src/game/Terrains/Cemetery.java
@@ -9,24 +9,43 @@ import game.Enemies.Undead;
 
 import java.util.Random;
 
+/**
+ *
+ */
 public class Cemetery extends Ground {
     private GameMap map;
     private Player player;
 
+    /**
+     * Constructor for cemetery, displayed a 'c' on map
+     */
     public Cemetery() {
         super('c');
     }
 
+    /**
+     * implements impassable terrain, or terrain that is only passable if conditions are met.
+     * @param actor the Actor to check
+     * @return true
+     */
     @Override
     public boolean canActorEnter(Actor actor) {
         return false;
     }
 
+    /**
+     * returns a string representing the Cemetery class
+     * @return string representing Cemetery class
+     */
     @Override
     public String toString() {
         return "Cemetery";
     }
 
+    /**
+     * Method is called every turn, has a 25% chance to spawn an undead
+     * @param location The location of the Ground
+     */
     @Override
     public void tick(Location location) {
         super.tick(location);
