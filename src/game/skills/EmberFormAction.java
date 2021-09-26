163a0241364e838fe9c1e54a2f0d7f0a1cfa1c3d
yoo <ykog0002@student.monash.edu>
2021-09-26 20:46:16 +0900

    Javadocs for All WeaponActions

--- src/game/skills/EmberFormAction.java
+++ src/game/skills/EmberFormAction.java
@@ -6,7 +6,8 @@ import game.Fire;
 
 public class EmberFormAction extends WeaponAction{
     /**
-     * Constructor
+     * Constructor for EmberFormAction, which is triggered by YhormsGreatMachete, and this inherits
+     * from WeaponAction
      *
      * @param weaponItem the weapon item that has capabilities
      */
@@ -14,6 +15,16 @@ public class EmberFormAction extends WeaponAction{
         super(weaponItem);
     }
 
+    /**
+     * Only Yhorm can hold Machete, and hence able to execute this WeaponAction.
+     * This turns adjacent dirt into fire for 4 turns which deals 25 to non-Yhorm.
+     * This is done by adding item to the ground.
+     *
+     * @see Fire for how we implemented this.
+     * @param actor Actor to execute this action
+     * @param map GameMap where this actor lies
+     * @return String message to print after this action is done
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         for (Exit exits: map.locationOf(actor).getExits()) {
@@ -22,6 +33,7 @@ public class EmberFormAction extends WeaponAction{
                 exits.getDestination().addItem(new Fire());
             }
 
+            // This comment is my old version of not using Exits.
 //        floor = new Location(map, x, y+1);
 //        if (floor.getGround().getDisplayChar() == '.'){
 //            floor.addItem(new Fire());
@@ -39,6 +51,11 @@ public class EmberFormAction extends WeaponAction{
         return "Ember Form activated! Yhorm The Giant uses Burn Ground!";
     }
 
+    /**
+     * Description to show to the menu
+     * @param actor Actor who is performing this action
+     * @return String message to show the menu, telling that the floor is burnt
+     */
     @Override
     public String menuDescription(Actor actor) {
         return "Yhorm would burn adjacent floors";
