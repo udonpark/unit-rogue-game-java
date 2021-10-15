1f2d6d643963bfd3937d386464997a87ab05f402
yoo <ykog0002@student.monash.edu>
2021-10-15 22:57:14 +0900

    Added CindersOfLord package and Cinders for 2 enemies

---
+++ src/game/CindersOfLord/CinderOfAldrich.java
@@ -0,0 +1,16 @@
+package game.CindersOfLord;
+
+public class CinderOfAldrich extends CindersOfLord {
+
+    /***
+     * Constructor.
+     * @param name the name of this Item
+     * @param displayChar the character to use to represent this item if it is on the ground
+     * @param portable true if and only if the Item can be picked up
+     */
+    public CinderOfAldrich(String name, char displayChar, boolean portable) {
+        super("CinderOfLord(Aldrich)", 'C', true);
+    }
+
+
+}
