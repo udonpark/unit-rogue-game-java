f0e25096f768a85223089dd6feb6d1d44632da76
hriz0002 <hriz0002@student.monash.edu>
2021-10-17 20:51:45 +0800

    Javadocs for chest

--- src/game/Chest/Chest.java
+++ src/game/Chest/Chest.java
@@ -12,18 +12,33 @@ public class Chest extends Actor {
      */
     private Location location;
 
-
+    /**
+     *
+     * @param name
+     * @param x
+     * @param y
+     * @param Map
+     */
     public Chest(String name, int x, int y, GameMap Map) {
         super(name, '?', 0);
         location = new Location(Map,x,y);
     }
 
-
+    /**
+     *
+     * @return
+     */
     public Location getLocation() {
         return location;
     }
 
-
+    /**
+     *
+     * @param otherActor the Actor that might be performing attack
+     * @param direction  String representing the direction of the other Actor
+     * @param map        current GameMap
+     * @return
+     */
     @Override
     public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
         Actions actions = new Actions();
@@ -34,7 +49,15 @@ public class Chest extends Actor {
         return actions;
     }
 
-
+    /**
+     *
+     * @param actions    collection of possible Actions for this Actor
+     * @param lastAction The Action this Actor took last turn. Can do interesting things in conjunction with Action.getNextAction()
+     * @param map        the map containing the Actor
+     * @param display    the I/O object to which messages may be written
+     * @return
+     * It stays at its place so does nothing
+     */
     @Override
     public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
         return new DoNothingAction();
